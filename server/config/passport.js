43cd43a6d0dd0662cf3052628843c20ed6f3991f
Parth Chauhan <universetcenter@gmail.com>
2026-02-18 21:40:12 +0530

    fix: Enable proxy support for Google OAuth on Vercel

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,6 +8,7 @@ module.exports = (passport) => {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
                 callbackURL: '/api/auth/google/callback',
+                proxy: true,
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
